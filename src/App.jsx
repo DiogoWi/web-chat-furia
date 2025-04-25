586793b5f0f2b1cf3dba243a801e5c32a778adad
DiogoWi <diogowillian499@gmail.com>
2025-04-25 13:53:57 -0300

    adição dos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Chat from './component/chat'
+import Lista from './component/lista'
 
 function App() {
   return (
     <div className="container">
+      <Lista />
+      <Chat />
     </div>
   )
 }
