927865955d78a9a325eb0eece8dae9e1fea8a911
DiogoWi <diogowillian499@gmail.com>
2025-04-27 14:25:12 -0300

    servidor socket-io iniciado

---
+++ server/src/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import { Server, createServer } from 'http';
+import { Server as Io } from 'socket.io';
+
+class App {
+    public app: express.Application;
+    public server: Server;
+    private socketIo: Io;
+
+    constructor() {
+        this.app = express();
+        this.server = createServer(this.app);
+        this.socketIo = new Io(this.server, {
+            cors: {
+                origin: '*'
+            }
+        });
+
+        this.socketIo.on('connection', socket => {
+            console.log('conectou')
+
+            socket.on('mensagem', mensagem => {
+                this.socketIo.emit('mensagem', mensagem)
+            })
+        })
+    }
+}
+
+export default App;
